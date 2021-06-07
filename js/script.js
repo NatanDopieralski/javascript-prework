f48b9bf3982721520f3e1c703a32cda933e11c48
Natan Dopieralski <natandopieralski@gmail.com>
2021-06-07 19:59:56 +0200

    add score in scripts.js

--- js/script.js
+++ js/script.js
@@ -31,3 +31,26 @@ if(playerInput == '1'){
 printMessage('Twój ruch to: ' + playerMove);
 
 
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!(gracz)');
+  } else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!(gracz)');
+  } else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!(gracz)');
+  } else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!(komputer)');
+  } else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!(komputer)');
+  } else if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!(komputer)');
+  } else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('remis');
+  } else if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('remis');
+  } else if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('remis');
+  } else if(playerMove > '3'){
+    printMessage('zła liczba');
+  } 
+
+
